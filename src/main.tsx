21e51db7e7766363584e8b501ebe735b3991eaca
Keto K. <kirua@duck.com>
2025-08-09 18:48:49 -0600

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PortfolioTerminal from './PortfolioTerminal'
+import './index.css' // Only if you already have global styles
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <PortfolioTerminal />
+  </React.StrictMode>
+)
